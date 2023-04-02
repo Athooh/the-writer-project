525073e52adf1b41fa8a62f99277338d2a137e6a
odhiambo_athooh <s.athooh@gmail.com>
2023-04-03 01:37:37 +0300

    added database, pages app and static files

--- the_writer/settings.py
+++ the_writer/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'pages.apps.PagesConfig',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
